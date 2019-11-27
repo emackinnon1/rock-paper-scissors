8b4cc7e66346982cdebe76bb6377a8b577e8314a
emackinnon1 <emackinnon100390@gmail.com>
2019-11-27 10:22:57 -0500

    add 5 round game and result announcement

--- rps.js
+++ rps.js
@@ -2,6 +2,7 @@ let playerChoice;
 let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
+let roundCount = 0;
 
 const score = document.querySelector('#score');
 
@@ -12,11 +13,15 @@ score.appendChild(scoreboard);
 const result = document.createElement('div');
 result.id = 'result';
 score.appendChild(result);
-    
+
+const buttons = document.querySelectorAll('button');
+
+const buttonDiv = document.getElementById('buttons');
+
 
     function computerPlay() {
         let random = Math.random();
-        
+
         if (random < 0.3) {
             computerSelection = 'rock';
         } else if (random > 0.3 && random < 0.6) {
@@ -29,19 +34,21 @@ score.appendChild(result);
 
     function oneRound(playerChoice, computerChoice) {
 
+
       if (playerChoice === computerChoice) {
         document.getElementById('result').textContent = 'It is a tie!';
-      } 
-      
+        roundCount++;
+      }
+
       else if (playerChoice === 'rock') {
         if (computerChoice === 'scissors') {
             playerScore++;
           document.getElementById('result').textContent = 'Rock beats scissors. Human wins!';
-          
+          roundCount++;
         } else {
           computerScore++;
           document.getElementById('result').textContent = 'Paper beats rock. Computer wins!';
-          
+          roundCount++;
         }
       }
 
@@ -49,11 +56,11 @@ score.appendChild(result);
         if (computerChoice === 'rock') {
           playerScore++;
           document.getElementById('result').textContent = 'Paper beats rock. Human wins!';
-          
+          roundCount++;
         } else {
           computerScore++;
           document.getElementById('result').textContent = 'Scissors beats paper. Computer wins!';
-          
+          roundCount++;
         }
       }
 
@@ -61,27 +68,38 @@ score.appendChild(result);
         if (computerChoice === 'paper') {
           playerScore++;
           document.getElementById('result').textContent = 'Scissors beats paper. Human wins!';
-          
+          roundCount++;
         } else {
           computerScore++;
           document.getElementById('result').textContent = 'Rock beats scissors. Computer wins!';
-          
+          roundCount++;
         }
       }
-    
     }
 
+
       
-      const buttons = document.querySelectorAll('button');
-      buttons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            playerChoice = button.id;
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
+      playerChoice = button.id;
 
-            oneRound(playerChoice, computerPlay());
-            document.getElementById('scoreboard').textContent = `Computer: ${computerScore}
-            Human: ${playerScore}`;
-            
-        });
-      });
+      fullGame(oneRound(playerChoice, computerPlay()));
+      document.getElementById('scoreboard').textContent = `Computer: ${computerScore}
+      Human: ${playerScore}`;
+  });
+});
 
-    
+function fullGame() {
+  if (roundCount > 4) {
+    buttonDiv.style.display = 'none';
+
+    if (playerScore > computerScore) {
+    result.textContent = `Human wins game with ${playerScore} points!`;
+  } else if (playerScore < computerScore) {
+    result.textContent = `Computer wins game with ${computerScore} points!`;
+  } else if (playerScore === computerScore) {
+    result.textContent = `It's a tie!`;
+  }
+ }
+ return;
+}
